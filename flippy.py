c3d7b29bd951a5b316743aaca8bde6eb1f90671a
Sydney N <git@krugg.com>
2018-07-26 11:42:53 -0700

    working manual board

--- flippy.py
+++ flippy.py
@@ -16,9 +16,11 @@ WINDOWHEIGHT = 480 # height in pixels
 SPACESIZE = 50 # width & height of each space on the board, in pixels
 BOARDWIDTH = 8 # how many columns of spaces on the game board
 BOARDHEIGHT = 8 # how many rows of spaces on the game board
-WHITE_CIRCLE = 'WHITE_CIRCLE' # an arbitrary but unique value
-BLACK_CIRCLE= 'BLACK_CIRCLE' # an arbitrary but unique value
-EMPTY_SPACE = 'EMPTY_SPACE' # an arbitrary but unique value
+PLAYER1 = 'PLAYER1' # an arbitrary but unique value
+PLAYER2= 'PLAYER2' # an arbitrary but unique value
+EMPTY_SPACE = 'EMPTY_SPACE'
+PLAYER1_CROWNED = 'PLAYER1_CROWNED'
+PLAYER2_CROWNED = 'PLAYER2_CROWNED'# an arbitrary but unique value
 
 # Amount of space on the left & right side (XMARGIN) or above and below
 # (YMARGIN) the game board, in pixels.
@@ -33,6 +35,7 @@ BRIGHTBLUE = (  0,  50, 255)
 BROWN      = (174,  94,   0)
 RED        = (203,  32,  39)
 TAN        = (222, 188, 153)
+PINK       = (255, 105, 180)
 TEXTBGCOLOR1 = BRIGHTBLUE
 TEXTBGCOLOR2 = GREEN
 GRIDLINECOLOR = BLACK
@@ -78,23 +81,32 @@ def runGame():
 
     # Draw the starting board and ask the player what color they want.
     drawBoard(mainBoard,movepiece)
-    playerTile, computerTile = [WHITE_CIRCLE,BLACK_CIRCLE]
+    playerTile, computerTile = [PLAYER1,PLAYER2]
 
     # Make the Surface and Rect objects for the "New Game"
     newGameSurf = FONT.render('New Game', True, TEXTCOLOR, TEXTBGCOLOR2)
     newGameRect = newGameSurf.get_rect()
     newGameRect.topright = (WINDOWWIDTH - 8, 10)
+    
+    deleteSurf = FONT.render('Delete', True, TEXTCOLOR, TEXTBGCOLOR2)
+    deleteRect = deleteSurf.get_rect()
+    deleteRect.topright = (WINDOWWIDTH - 8, 50)
+    
+    crownSurf = FONT.render('Crown', True, TEXTCOLOR, TEXTBGCOLOR2)
+    crownRect = crownSurf.get_rect()
+    crownRect.topright = (WINDOWWIDTH - 8, 90)
 
     while True: # main game loop
+        movepiece = None
         # Draw the game board.
         drawBoard(mainBoard,movepiece)
         # Draw the "New Game" and "Hints" buttons.
         DISPLAYSURF.blit(newGameSurf, newGameRect)
+        
         MAINCLOCK.tick(FPS)
         pygame.display.update()
-
-        movexy = None
-        while movexy == None:
+        movefromxy = None
+        while movepiece == None:
             # Keep looping until the player clicks on a valid space.
             checkForQuit()
             for event in pygame.event.get(): # event handling loop
@@ -105,11 +117,50 @@ def runGame():
                         # Start a new game
                         return True
                     # movexy is set to a two-item tuple XY coordinate, or None value
+                    movefromxy = getSpaceClicked(mousex, mousey)
+                    if movefromxy != None and not isValidMove(mainBoard, playerTile, movefromxy[0], movefromxy[1]):
+                        movefromxy = None
+                    if movefromxy != None and mainBoard[movefromxy[0]][movefromxy[1]] != EMPTY_SPACE:
+                        movepiece = movefromxy
+                        
+        drawBoard(mainBoard,movepiece)
+        DISPLAYSURF.blit(newGameSurf, newGameRect)
+        DISPLAYSURF.blit(deleteSurf, deleteRect)
+        DISPLAYSURF.blit(crownSurf, crownRect)
+        MAINCLOCK.tick(FPS)
+        pygame.display.update()
+        movexy = None
+        isDone = False
+        while movexy == None and not isDone:
+            # Keep looping until the player clicks on a valid space.
+            checkForQuit()
+            for event in pygame.event.get(): # event handling loop
+                if event.type == MOUSEBUTTONUP:
+                    # Handle mouse click events
+                    mousex, mousey = event.pos
+                    if newGameRect.collidepoint( (mousex, mousey) ):
+                        # Start a new game
+                        return True
+                    if deleteRect.collidepoint( (mousex, mousey) ):
+                        mainBoard[movefromxy[0]][movefromxy[1]] = EMPTY_SPACE
+                        isDone = True
+                    if crownRect.collidepoint( (mousex, mousey) ):
+                        #centerx, centery = translateBoardToPixelCoord(x, y)
+                        if mainBoard[movefromxy[0]][movefromxy[1]] == PLAYER1 or mainBoard[movefromxy[0]][movefromxy[1]] == PLAYER2:
+                            if mainBoard[movefromxy[0]][movefromxy[1]] == PLAYER1:
+                                mainBoard[movefromxy[0]][movefromxy[1]] = PLAYER1_CROWNED
+                            else:
+                                mainBoard[movefromxy[0]][movefromxy[1]] = PLAYER2_CROWNED
+                        isDone = True
+                        #pygame.draw.circle(DISPLAYSURF, tileColor, (centerx, centery), int(SPACESIZE / 2) - 4)
+
+                    # movexy is set to a two-item tuple XY coordinate, or None value
                     movexy = getSpaceClicked(mousex, mousey)
-                    if movexy != None and not isValidMove(mainBoard, playerTile, movexy[0], movexy[1]):
+                    if movexy == None or not isValidMove(mainBoard, playerTile, movexy[0], movexy[1]) or mainBoard[movexy[0]][movexy[1]] != EMPTY_SPACE:
                         movexy = None
-                    if movexy != None and mainBoard[movexy[0]][movexy[1]] != EMPTY_SPACE:
-                        movepiece = movexy
+        if movexy != None:                
+            mainBoard[movexy[0]][movexy[1]] = mainBoard[movefromxy[0]][movefromxy[1]]
+            mainBoard[movefromxy[0]][movefromxy[1]] = EMPTY_SPACE
                         
 def translateBoardToPixelCoord(x, y):
     return XMARGIN + x * SPACESIZE + int(SPACESIZE / 2), YMARGIN + y * SPACESIZE + int(SPACESIZE / 2)
@@ -133,12 +184,17 @@ def drawBoard(board,movepiece):
             
     for x in range(BOARDWIDTH):
         for y in range(BOARDHEIGHT):
+            print("x:", x, ", y:", y, board[x][y])
             centerx, centery = translateBoardToPixelCoord(x, y)
-            if board[x][y] == WHITE_CIRCLE or board[x][y] ==BLACK_CIRCLE:
-                if board[x][y] == WHITE_CIRCLE:
+            if board[x][y] == PLAYER1 or board[x][y] ==PLAYER2 or board[x][y] == PLAYER1_CROWNED or board[x][y] == PLAYER2_CROWNED:
+                if board[x][y] == PLAYER1:
                     tileColor = RED
-                else:
+                elif board[x][y] == PLAYER2:
                     tileColor = TAN
+                elif board[x][y] == PLAYER1_CROWNED:
+                    tileColor = PINK
+                else:
+                    tileColor = WHITE
                 pygame.draw.circle(DISPLAYSURF, tileColor, (centerx, centery), int(SPACESIZE / 2) - 4)
                 if movepiece != None and movepiece [0] == x and movepiece [1] == y:
                     pygame.draw.rect(DISPLAYSURF, RED, (centerx - 4, centery - 4, 8, 8))
@@ -162,31 +218,31 @@ def resetBoard(board):
             board[x][y] = EMPTY_SPACE
 
     # Add starting pieces to the center
-    board[0][0] = WHITE_CIRCLE
-    board[0][2] = WHITE_CIRCLE
-    board[0][4] = WHITE_CIRCLE
-    board[0][6] = WHITE_CIRCLE
-    board[1][1] = WHITE_CIRCLE
-    board[1][3] = WHITE_CIRCLE
-    board[1][5] = WHITE_CIRCLE
-    board[1][7] = WHITE_CIRCLE
-    board[2][0] = WHITE_CIRCLE
-    board[2][2] = WHITE_CIRCLE
-    board[2][4] = WHITE_CIRCLE
-    board[2][6] = WHITE_CIRCLE
+    board[0][0] = PLAYER1
+    board[0][2] = PLAYER1
+    board[0][4] = PLAYER1
+    board[0][6] = PLAYER1
+    board[1][1] = PLAYER1
+    board[1][3] = PLAYER1
+    board[1][5] = PLAYER1
+    board[1][7] = PLAYER1
+    board[2][0] = PLAYER1
+    board[2][2] = PLAYER1
+    board[2][4] = PLAYER1
+    board[2][6] = PLAYER1
     
-    board[5][1] = BLACK_CIRCLE
-    board[5][3] = BLACK_CIRCLE
-    board[5][5] = BLACK_CIRCLE
-    board[5][7] = BLACK_CIRCLE
-    board[6][0] = BLACK_CIRCLE
-    board[6][2] = BLACK_CIRCLE
-    board[6][4] = BLACK_CIRCLE
-    board[6][6] = BLACK_CIRCLE
-    board[7][1] = BLACK_CIRCLE
-    board[7][3] = BLACK_CIRCLE
-    board[7][5] = BLACK_CIRCLE
-    board[7][7] = BLACK_CIRCLE
+    board[5][1] = PLAYER2
+    board[5][3] = PLAYER2
+    board[5][5] = PLAYER2
+    board[5][7] = PLAYER2
+    board[6][0] = PLAYER2
+    board[6][2] = PLAYER2
+    board[6][4] = PLAYER2
+    board[6][6] = PLAYER2
+    board[7][1] = PLAYER2
+    board[7][3] = PLAYER2
+    board[7][5] = PLAYER2
+    board[7][7] = PLAYER2
 
 
 def getNewBoard():
